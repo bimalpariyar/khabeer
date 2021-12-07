f51454cd0ade125f1b59f154b57a415a63d762c2
bimalpariyar1@gmail.com <bimalpariyar1@gmail.com>
2021-12-07 17:33:12 +0545

    forms developed

---
+++ js/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  //Click event to scroll to top
+  // $(".back-to-top").click(function () {
+  //   $("html, body").animate({ scrollTop: 0 }, 500);
+  //   return false;
+  // });
+
+  $(".side-drawer-content").css({ height: window.innerHeight + "px" });
+
+  //Hamburger Menu Click event
+
+  $(".ham-meunu").click(function () {
+    console.log("open clicked");
+    $("body").css({ overflow: "hidden" });
+    $(".side-drawer-menu").css({ left: "0" });
+    $(".openmenu-backdrop").toggle();
+  });
+
+  $(".drawer-menu-close-icon").click(function () {
+    console.log("close clicked");
+    $("body").css({ overflow: "" });
+    $(".side-drawer-menu").css({ left: "-100%" });
+    $(".openmenu-backdrop").toggle();
+  });
+
+  $(".create-req-btn").click(function () {
+    $(".create-request-popup").css({
+      display: "block",
+    });
+    $("body").css({ overflow: "hidden" });
+  });
+
+  $(".create-request-popup .close-button").click(function () {
+    $(".create-request-popup").css({
+      display: "none",
+    });
+    $("body").css({ overflow: "" });
+  });
+
+  assingHeaderSpacerHeight();
+});
+
+$(window).resize(function () {
+  $(".side-drawer-content").css({ height: window.innerHeight + "px" });
+
+  assingHeaderSpacerHeight();
+});
+
+function assingHeaderSpacerHeight() {
+  var headerheight = $(".site-header").outerHeight();
+
+  $(".header-spacer").css({
+    height: headerheight + "px",
+  });
+}
